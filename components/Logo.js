4c34e2570d70aafa2c31c283eb4e45309a939315
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-22 17:16:00 -0700

    feat: create Logo

---
+++ components/Logo.js
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+// Assets
+import treeIcon from '../public/images/tree-icon.svg';
+
+const StyledText = styled.p`
+  font-family: var(--fnt-bold);
+  font-size: 1.1rem;
+`;
+
+function Img({ className }) {
+  return (
+    <div className={className}>
+      <Image src={treeIcon} alt="A tree icon" layout="fill" objectFit="cover"/>
+    </div>
+  );
+}
+
+const StyledImage = styled(Img)`
+  position: relative;
+  height: 40px;
+  width: 40px;
+`;
+
+function Logo({ className }) {
+  return (
+    <div className={className}>
+      <StyledImage />
+      <StyledText>redwoods.hum</StyledText>
+    </div>
+  );
+}
+
+const StyledLogo = styled(Logo)`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export default StyledLogo;
