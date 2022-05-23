0d1a407302ce681e49dac54bc56a9a712ded20d8
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-23 16:28:25 -0700

    feat: add sign out feature

--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,65 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
+// Firebase
+import { auth } from '../firebaseConfig';
+import { signOut } from "firebase/auth";
 
 // Components
 import Logo from './Logo';
+import { StyledBtn2 } from '../components/Btns';
 
 const StyledText = styled.p`
+  display: block;
+  ${({ welcomeVisible }) => !welcomeVisible && 'display: none;'}
   font-family: var(--fnt-bold);
 `;
 
+const StyledSignOutBtnCont = styled.div`
+  display: none;
+  ${({ welcomeVisible }) => !welcomeVisible && 'display: block;'}
+`;
+
 function UserNameCont({ className, userName }) {
+  const router = useRouter();
+  const [welcomeVisible, setWelcomeVisible] = useState(true);
+
+  const signOutUser = () => {
+    signOut(auth).then(() => {
+      router.push('/');
+    })
+    .catch((err) => {
+      console.error(`Failed to sign out user: ${err}`);
+    });
+  }
+
   return (
     <div className={className}>
-      <StyledText>Welcome {userName}</StyledText>
+      <StyledText
+        welcomeVisible={welcomeVisible}
+        onMouseEnter={() => setWelcomeVisible(false)}
+      >
+          Welcome {userName}
+      </StyledText>
+      <StyledSignOutBtnCont
+        onMouseLeave={() => setWelcomeVisible(true)}
+        welcomeVisible={welcomeVisible}
+      >
+        <StyledBtn2
+          type="button"
+          clickHandler={signOutUser}
+          text="Sign out"
+        />
+      </StyledSignOutBtnCont>
     </div>
   );
 }
 
 const StyledUserNameCont = styled(UserNameCont)`
   display: none;
-  ${({ dashboard }) => dashboard && 'display: block;'}
+  ${({ dashboard }) => dashboard && 'display: flex;'}
+  cursor: pointer;
 `;
 
 function Nav({ className, userName, dashboard }) {
