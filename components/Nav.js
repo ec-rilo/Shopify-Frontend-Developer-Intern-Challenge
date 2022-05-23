7c337a686a9918ee9b7f156af0fe846ccc6e077a
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-23 00:38:11 -0700

    feat: add user's name on sign in

--- components/Nav.js
+++ components/Nav.js
@@ -3,16 +3,36 @@ import styled from 'styled-components';
 // Components
 import Logo from './Logo';
 
-function Nav({ className }) {
+const StyledText = styled.p`
+  font-family: var(--fnt-bold);
+`;
+
+function UserNameCont({ className, userName }) {
+  return (
+    <div className={className}>
+      <StyledText>Welcome {userName}</StyledText>
+    </div>
+  );
+}
+
+const StyledUserNameCont = styled(UserNameCont)`
+  display: none;
+  ${({ dashboard }) => dashboard && 'display: block;'}
+`;
+
+function Nav({ className, userName, dashboard }) {
   return (
     <nav className={className}>
       <Logo />
+      <StyledUserNameCont userName={userName} dashboard={dashboard && userName}/>
     </nav>
   );
 }
 
 const StyledNav = styled(Nav)`
-
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export default StyledNav;
