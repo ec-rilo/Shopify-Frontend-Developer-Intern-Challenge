472ce2f04554a2e8dd5efa9fb692dc2c36018596
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-23 00:18:17 -0700

    feat: add authentication

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
+import { useRouter } from 'next/router'
 import styled from 'styled-components';
 
-// Note: Cont is short for Container
+// Firebase
+import { auth } from '../firebaseConfig';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 // Components
 import StyledLogin from './Login';
+import StyledDashboard from './Dashboard';
 
 export default function Home() {
+  const router = useRouter();
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      router.push('/Dashboard');
+    }
+  }, [user, router]);
+
   return (
     <div>
       <Head>
         <title>Fun with GPT-3</title>
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
-        <StyledLogin />
+      <StyledLogin />
     </div>
   )
 }
