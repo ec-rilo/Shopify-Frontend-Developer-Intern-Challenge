73ba453a8ee47039cb2fb90ee982223da09d6f5a
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-25 08:43:40 -0700

    feat: implement socket.io

--- server/index.js
+++ server/index.js
@@ -4,18 +4,27 @@ const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
+const nextHandler = nextApp.getRequestHandler()
 
 nextApp.prepare().then(() => {
   const app = express();
 
+  // socket.io
+  const server = require('http').createServer(app);
+  const io = require('socket.io')(server);
+
+  io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+    });
+  });
+
   app.all('*', (req, res) => {
-    return handle(req, res)
+    return nextHandler(req, res)
   })
 
   const PORT = process.env.SERVER_PORT || 3000;
 
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`> Ready on http://localhost:${PORT}`)
   })
 })
